9702e375c71b0eee50655244f0644d47d849c366
rog-mccoy <rogmccoy@outlook.com>
2021-05-31 17:51:26 -0400

    Table logic added to Index.cshtml and site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,3 +3,21 @@
 
 // Write your Javascript code.
 console.log(`Hello World`)
+const data = [
+    { "pc_number": "PC13440" },
+    { "pc_number": "Halo1"}
+]
+const cortex_table = document.getElementById("cortex_table");
+const table_body = cortex_table.getElementsByTagName("tbody")[0];
+
+data.forEach((element, index) => {
+    console.log(`index: ${index}, value: ${element.pc_number}`);
+    let row = table_body.insertRow(index);
+    for(let prop in element) {
+        let cell0 = row.insertCell(0);
+        let cell1 = row.insertCell(1);
+
+        cell0.innerHTML = prop
+        cell1.innerHTML = element[prop]        
+    }
+});
