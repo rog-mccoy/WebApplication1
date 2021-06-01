b4311ab10a56560ed1d42191f13438a62694d609
rog-mccoy <rogmccoy@outlook.com>
2021-06-01 19:46:19 -0400

    updated cortex data and table creation logic

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,19 +4,22 @@
 // Write your Javascript code.
 console.log(`Hello World`)
 const data = [
-    { "pc_number": "PC13440" },
-    { "pc_number": "Halo1"}
+    {
+        "pc_number": "PC13440",
+        "ip_address": "10.10.10.10",
+        "user_name": "roger"
+    }
 ]
 const cortex_table = document.getElementById("cortex_table");
 const cortex_table_body = cortex_table.getElementsByTagName("tbody")[0];
 
 data.forEach((element, index) => {
     console.log(`index: ${index}, value: ${element.pc_number}`);
-    let row = cortex_table_body.insertRow(index);
-    for(let prop in element) {
+    for (let prop in element) {
+        let row = cortex_table_body.insertRow(index);
         let cell = row.insertCell();
         cell.innerHTML = prop
         cell = row.insertCell();
         cell.innerHTML = element[prop]
-    }
+    }    
 });
